831fc0aae1d341d75aa910f9d8e21cf7306666f0
ifti-77 <23-51177-1@student.aiub.edu>
2026-02-15 15:25:13 +0600

    fixed module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
+import { AccountantModule } from './accountant/ccountant.module';
+import { ManagerModule } from './manager/manager.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule,AccountantModule,ManagerModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
