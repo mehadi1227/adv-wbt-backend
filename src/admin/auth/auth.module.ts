5af707cb90ffd3e93ce19fcbab08baed9702daad
ifti-77 <23-51177-1@student.aiub.edu>
2026-04-07 12:02:53 +0600

    changed auth module directory

---
+++ src/admin/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './auth.constant';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserEntity } from '../admin/entities/user.entity';
+import { AuthGuard } from './auth.guard';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity]),
+  JwtModule.register({
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '1h' },
+  })],
+  controllers: [AuthController],
+  providers: [AuthService, AuthGuard],
+  exports: [ JwtModule],
+})
+export class AuthModule {}
